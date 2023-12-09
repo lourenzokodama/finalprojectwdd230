8fddb6b01d402c4f052dca6208c982ca313ce9a3
lourenzokodama <19721070+lourenzokodama@users.noreply.github.com>
2023-12-09 03:33:55 -0700

    Update weather.js

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,33 +1,23 @@
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
-
-const url = `https://api.openweathermap.org/data/2.5/weather?zip=97741,us&units=imperial&appid=33e21c071ddd9d8ad6330516b69de4b7`;
-
-apiFetch(url);
-
-async function apiFetch(apiURL) {
-  try {
-    const response = await fetch(apiURL);
-    if (response.ok) {
-      const data = await response.json();
-      displayResults(data);
-    } else {
-        throw Error(await response.text());
-    }
-  } catch (error) {
-      console.log(error);
-  }
+const api_key = "30dab8ad616006464c6fcefabccd5254"
+const imgURL = "https://openweathermap.org/img/w/"
+const k2f = (K) => {
+    return (K - 273.15)* 1.8000 + 32.00
+}
+const rnd = (n,d) => {
+    return (Math.round(n*10**d)/10**d)
 }
 
+fetch('https://api.openweathermap.org/data/2.5/weather?id=5604473&appid='+api_key)
+    .then(res => {
+        return res.json()
+    })
+    .then(jsonData => {
+        console.log(jsonData)
+        document.querySelector('#current-temp').textContent = rnd(k2f(jsonData.main.temp),1)
 
-function  displayResults(weatherData) {
-  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-
-  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-  const desc = toTitleCase(weatherData.weather[0].description);
-
-  weatherIcon.setAttribute('src', iconsrc);
-  weatherIcon.setAttribute('alt', desc);
-  captionDesc.textContent = desc;
-}
+        let imageURL = imgURL+jsonData.weather[0].icon+'.png'
+        let imageDesc = jsonData.weather[0].description
+        document.querySelector('#imagesrc').textContent = imageURL
+        document.querySelector('#icon').setAttribute('src',imageURL)
+        document.querySelector('#icon').setAttribute('alt',imageDesc)
+    })
